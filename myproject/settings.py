6692ab750ff6952608b23206e5b113a4c5d8fa46
Michael Abramov <ma879w@gmail.com>
2025-11-27 11:12:40 -0500

    upd allowed hosts in settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -32,7 +32,9 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+  ".onrender.com",
+]
 
 
 # Application definition
